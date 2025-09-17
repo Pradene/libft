3eb958f6e88d630d5793c4b36513993f0e6c1c86
Liam Pradene <81440101+Pradene@users.noreply.github.com>
2025-09-17 18:53:10 +0200

    Update gnl

--- src/gnl/gnl.c
+++ src/gnl/gnl.c
@@ -4,109 +4,105 @@
 # define BUFFER_SIZE 128
 #endif
 
-void	add_to_stash(char **stash, char *buf, int readc) {
+static void	add_to_stash(char **stash, char *buf, int readc) {
 	char	*s;
+	int		size;
 
-	s = malloc(sizeof(char) * (ft_strlen(*stash) + readc + 1));
-	if (!s) {
+	size = (int)ft_strlen((*stash));
+	s = malloc(sizeof(char) * (size + readc + 1));
+	if (s == NULL)
 		return ;
-    }
-	ft_memcpy(s, *stash, ft_strlen(*stash));
-	ft_memcpy(s + ft_strlen(*stash), buf, readc + 1);
-	free(*stash);
-	*stash = s;
+	if ((*stash) != NULL) {
+		ft_memcpy(s, (*stash), size);
+		free((*stash));
+	}
+	ft_memcpy(s + size, buf, readc);
+	s[size + readc] = '\0';
+	(*stash) = s;
 }
 
-void	read_buf(char **stash, int fd) {
-	char	*buf;
+static void	read_file(char **stash, int fd) {
+	char	buf[BUFFER_SIZE + 1];
 	int		readc;
 
-	readc = 1;
-	while (!ft_strchr(*stash, '\n') && readc != 0) {
-		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!buf) {
-			return ;
-        }
+	while (ft_strchr(*stash, '\n') == NULL) {
 		readc = read(fd, buf, BUFFER_SIZE);
-		if ((*stash == NULL && readc == -1) || readc == 0) {
-			free(buf);
+		if (readc <= 0) {
+			if (readc == -1 && (*stash) != NULL) {
+				free((*stash));
+				(*stash) = NULL;
+			}
 			return ;
 		}
 		buf[readc] = '\0';
 		add_to_stash(stash, buf, readc);
-		free(buf);
+		if ((*stash) == NULL) {
+			return ;
+		}
 	}
 }
 
-void	extract_line(char **line, char *stash) {
+static void	make_line(char **line, char *stash) {
 	int	i;
 
-	if (!stash) {
+	(*line) = NULL;
+	if (stash == NULL)
 		return ;
-    }
 	i = 0;
-	while (stash[i] != '\n' && stash[i]) {
+	while (stash[i] != '\0' && stash[i] != '\n')
 		i++;
-    }
-	if (stash[i] == '\n') {
+	if (stash[i] == '\n')
 		i++;
-    }
 	*line = malloc(sizeof(char) * (i + 1));
-	if (!(*line)) {
+	if ((*line) == NULL)
 		return ;
-    }
 	ft_memcpy(*line, stash, i);
 	(*line)[i] = '\0';
 }
 
-void	clean_stash(char **stash) {
+static void	clean_stash(char **stash) {
 	int		i;
 	int		j;
 	char	*s;
 
-	if (!(*stash)) {
+	if ((*stash) == NULL)
 		return ;
-    }
 	i = 0;
-	while ((*stash)[i] != '\n' && (*stash)[i]) {
+	while ((*stash)[i] != '\0' && (*stash)[i] != '\n')
 		i++;
-    }
-	if ((*stash)[i] == '\n') {
+	if ((*stash)[i] == '\n')
 		i++;
-    }
-	s = malloc(sizeof(char) * (ft_strlen(*stash) - i + 1));
-	if (!s) {
+	s = malloc(sizeof(char) * ((int)ft_strlen((*stash)) - i + 1));
+	if (s == NULL)
 		return ;
-    }
 	j = 0;
-	while ((*stash)[i]) {
+	while ((*stash)[i] != '\0') {
 		s[j] = (*stash)[i];
 		j++;
 		i++;
 	}
 	s[j] = '\0';
-	free(*stash);
-	*stash = s;
+	free((*stash));
+	(*stash) = s;
 }
 
 char	*get_next_line(int fd) {
-	static char	*stash;
+	static char	*stash = NULL;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0) {
-		return (0);
-    }
-	read_buf(&stash, fd);
-	if (!stash) {
-		return (0);
-    }
-	extract_line(&line, stash);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	read_file(&stash, fd);
+	if (!stash)
+		return (NULL);
+	make_line(&line, stash);
 	clean_stash(&stash);
 	if (line[0] == '\0') {
 		free(stash);
-		stash = 0;
+		stash = NULL;
 		free(line);
-		return (0);
+		return (NULL);
 	}
 	return (line);
 }
+
